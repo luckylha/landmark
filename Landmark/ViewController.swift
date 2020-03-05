1c5e7e418e964365d0e5fc945ad9ed04a4e3e9c9
Fransky Smooets <fransky@smooets.com>
2020-03-05 15:42:01 +0700

    Initial Commit

---
+++ Landmark/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Landmark
+//
+//  Created by lucky lukmanul on 05/03/20.
+//  Copyright © 2020 lucky lukmanul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
